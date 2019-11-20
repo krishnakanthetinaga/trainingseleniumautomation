905b9490fddf9f31c16e93ecd57eff0f928b99c7
akhilamaturi <maturiakhila@gmail.com>
2019-11-20 19:32:27 +0000

    add demo bb

---
+++ src/test/java/com/mavenit/selenium/training/RunCukesTest.java
@@ -0,0 +1,12 @@
+package com.mavenit.selenium.training;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources",
+        tags= "@search")
+        //strict = true, plugin = "json:target/cucumber.json",dryRun = true)
+public class RunCukesTest {
+}
